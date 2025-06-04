b4cc22bd9c1039c9f54c6b0fcf87cb224cc5659f
Hilman <hilmanrevanda@outlook.com>
2025-06-05 03:08:03 +0700

    add shippo api

---
+++ src/app/api/shippo/parcels/[id]/route.ts
@@ -0,0 +1,28 @@
+import { Shippo } from "shippo";
+import { NextRequest } from "next/server";
+
+const shippo = new Shippo({
+    apiKeyHeader: process.env.SHIPPO_API_TOKEN,
+    shippoApiVersion: "2018-02-08",
+});
+
+export const GET = async (req: NextRequest, context: { params: Promise<{ id: string }> }) => {
+  const id: string = (await context.params).id;
+  
+  try {
+    const result = await shippo.parcels.list(Number(id));
+
+    return new Response(
+      JSON.stringify({ data : result }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+  } catch (err: any) {
+    return new Response(
+      JSON.stringify({ error: err.message }),
+      { status: 500 }
+    )
+  }
+}
